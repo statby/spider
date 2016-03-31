f06d5d5cceb819e5977aa93ae115423fa8dc5f09
Statby <statby@gmail.com>
2016-03-31 20:13:55 +0800

    fix first page

--- top-site.py
+++ top-site.py
@@ -8,10 +8,17 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+
+
 data = []
-def get_data(page):
-    site = 'http://alexa.chinaz.com/Global/index_{}.html'.format(page)
-    req = requests.get(site)
+
+def get_data(page,area='Global'):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',}
+    if page == 1:
+        site = 'http://alexa.chinaz.com/Global/index.html'
+    else:
+        site = 'http://alexa.chinaz.com/Global/index_{}.html'.format(page)
+    req = requests.get(site,headers=headers)
     req.encoding = 'utf-8'
     status = req.status_code
     soup = BeautifulSoup(req.text, 'lxml')
@@ -26,6 +33,8 @@ def get_data(page):
         data.append([ranking.text, introduce.text, url.text])
     return(data)
 
+#print(get_data(1))
+
 #print (get_data(4))
 def get_more_page(start,stop):
     sum = []
@@ -33,18 +42,11 @@ def get_more_page(start,stop):
         sum += get_data(pages)
     return sum
 
+#print(get_more_page(1,3))
+
 #print(get_more_page(2,20))
-with open('top-site.txt', 'w+') as f:
-    f.write(str(get_more_page(2,20)))
+def write_down(start,stop):
+    with open('top-site.txt', 'w+') as f:
+        f.write(str(get_more_page(2,20)))
 
-
-'''
-
-for ranking in rankings:
-    print (ranking.text)
-for introduce in introduces:
-    print (introduce.text)
-for url in urls:
-    print (url.text)
-
-'''
+    
