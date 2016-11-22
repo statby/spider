2246ee7b3ef0432140191874f483c5320218c1e4
Statby <statby@gmail.com>
2016-11-23 00:20:34 +0800

    增加  house_spider 爬虫第一版

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #专门拿来练手的库
 ##top-website-ranking
     爬站长之家各区域排名前500的网站,并把相关信息写入到excel
@@ -16,4 +15,12 @@
 | get_area_url.p    |  或取不同区域列表 | 
 | get_global_site_ranking.py    |  默认获取全球网站排名并写入excel | 
 | get_different_area_site_ranking.py    |   获取不同区域排名情况并写入excel | 
->>>>>>> master
+
+
+## house_spider
+> **爬去广州二手房房价**
+
+
+>| 文件     |用途 |  
+| :-------- | :--------|
+| main.py | 房价爬虫第一版| 
---
+++ house_spider/main.py
@@ -0,0 +1,197 @@
+#!/bin/env python3
+# coding=utf-8
+# Date        : 2016-11-22 19:43:42
+# Author      : Statby
+# Description : 
+
+import random
+import requests
+from bs4 import BeautifulSoup
+import json
+import sqlite3
+import sys
+import re
+
+        
+#!/bin/env python3
+# coding=utf-8
+# Date        : 2016-11-22 19:43:42
+# Author      : Statby
+# Description :
+
+import random
+import requests
+from bs4 import BeautifulSoup
+import json
+import sqlite3
+import sys
+
+
+
+def randomheader():
+    # 随机返回http请求头
+    headers=[{'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'},\
+    {'User-Agent':'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},\
+    {'User-Agent':'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'},\
+    {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'},\
+    {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/44.0.2403.89 Chrome/44.0.2403.89 Safari/537.36'},\
+    {'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'},\
+    {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'},\
+    {'User-Agent':'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0'},\
+    {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1'},\
+    {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1'},\
+    {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11'},\
+    {'User-Agent':'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11'},\
+    {'User-Agent':'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11'}]
+    return random.choice(headers)
+
+def contents(url,timeout=4,encoding='gbk2312'):
+    r = requests.get(url,headers=randomheader(),timeout=timeout)
+    r.encoding = encoding
+    content = r.text
+    return content
+
+
+def lianjia_totalpage(content):
+    # 返回链接子页面下的总页数
+    soup = BeautifulSoup(content,'lxml') 
+    totalpage_tag  = soup.find(attrs={"class":"page-box house-lst-page-box"})
+    return (eval(totalpage_tag.get("page-data"))["totalPage"])
+    
+
+def lianjia_perpage_info(page):
+
+    conn = sqlite3.connect(db)
+
+    url = 'http://gz.lianjia.com/ershoufang/pg{page}'.format(page=page)
+    content = contents(url)
+    soup = BeautifulSoup(content,'lxml')
+    allhouse = soup.find_all('li',attrs={"class":"clear"})
+
+    for perhouse in allhouse:
+            perhouseinfo = {}
+#        try:
+            url = perhouse.find("a",attrs={"data-el":"ershoufang"}).get('href')
+            title = perhouse.find("img",attrs={"class":"lj-lazy"}).get('alt')
+
+            houseIcon = perhouse.find(attrs={"class":"houseInfo"}).text
+            district = houseIcon.split('|')[0].strip(" ")
+            types= houseIcon.split('|')[1].strip(" ")
+            size = houseIcon.split('|')[2].strip(" ") 
+            face = houseIcon.split('|')[3].strip(" ") 
+            decoration = houseIcon.split('|')[4].strip(" ") 
+
+            positionInfo =  re.split(' +',perhouse.find(attrs={"class":"positionInfo"}).text.strip('\n'))
+            floor = positionInfo[0].split('(')[0]
+            totalfloor = re.findall('\d+',positionInfo[0])[0]
+            try:
+                position = positionInfo[3]
+            except:
+                position = None
+            try:
+                buildtime = re.findall('\d+',positionInfo[1])[0]
+            except:
+                buildtime = None
+
+            followInfo = re.split(' / ',perhouse.find(attrs={"class":"followInfo"}).text)
+            attention = re.findall('\d+',followInfo[0])[0]
+            show = re.findall('\d+',followInfo[1])[0]
+            publishtime = followInfo[2]
+            
+            tag = perhouse.find(attrs={"class":"tag"}).text.strip(" ").strip('\n').split('\n')[0]
+
+            totalprice = re.findall('\d+',perhouse.find(attrs={"class":"totalPrice"}).text)[0]
+            unitprice = re.findall('\d+',perhouse.find(attrs={"class":"unitPrice"}).text)[0]
+
+    
+            perhouseinfo['url'] = url
+            perhouseinfo['title'] = title
+            perhouseinfo['district'] = district
+            perhouseinfo['types'] = types
+            perhouseinfo['size'] = size
+            perhouseinfo['face'] = face
+            perhouseinfo['decoration'] = decoration
+            perhouseinfo['floor'] = floor
+            perhouseinfo['totalfloor'] = totalfloor
+            perhouseinfo['position'] = position
+            perhouseinfo['buildtime'] = buildtime
+            perhouseinfo['attention'] = attention
+            perhouseinfo['show'] = show
+            perhouseinfo['publishtime'] = publishtime
+            perhouseinfo['tag'] = tag
+            perhouseinfo['totalprice'] = totalprice
+            perhouseinfo['unitprice'] = unitprice
+
+
+
+#            print (json.dumps(perhouseinfo,indent=1,ensure_ascii=False))
+#            格式化输出每个房源的所有信息
+    
+
+#            insertsql = 'INSERT INTO {tablename} (title,url,totalfloor,flood,houseinfo,addres,unitprice,totalprice,district,types,size,face,decoration,buildtime) \
+#                        VALUES ("{title}","{url}","{followinfo}","{flood}","{houseinfo}","{addres}","{unitprice}","{totalprice}","{district}","{types}","{size}","{face}","{decoration}","{buildtime}")'\
+#                        .format(tablename=TABLENAME,title=str(perhouseinfo["title"]),url=str(perhouseinfo["url"]),totalfloor=perhouseinfo["fotalfloor"],\
+#                        flood=perhouseinfo['flood'],houseinfo=perhouseinfo['houseinfo'],addres=perhouseinfo['addres'],unitprice=perhouseinfo['unitprice'],\
+#                        totalprice=perhouseinfo['totalprice'],district=perhouseinfo['district'],types=perhouseinfo['types'],size=perhouseinfo['size'],face=perhouseinfo['face'],decoration=perhouseinfo['decoration'],buildtime=perhouseinfo['buildtime'])
+#            conn.execute(insertsql)
+#        conn.commit()
+
+            insertsql = 'INSERT INTO {tablename} (url,title,district,types,size,face,decoration,floor,totalfloor,position,buildtime,attention,show,publishtime,tag,totalprice,unitprice) \
+                        VALUES ("{url}","{title}","{district}","{types}","{size}","{face}","{decoration}", \
+                        "{floor}","{totalfloor}","{position}","{buildtime}","{attention}","{show}","{publishtime}", \
+                        "{tag}","{totalprice}","{unitprice}") ' \
+                        .format(tablename=tablename,url=str(perhouseinfo["url"]),title=str(perhouseinfo["title"]),district=perhouseinfo['district'],\
+                        types=perhouseinfo['types'],size=perhouseinfo['size'],face=perhouseinfo['face'],decoration=perhouseinfo['decoration'],\
+                        floor=perhouseinfo['floor'],totalfloor=perhouseinfo["totalfloor"],position=perhouseinfo['position'],\
+                        buildtime=perhouseinfo['buildtime'],attention=perhouseinfo['attention'],show=perhouseinfo['show'],\
+                        publishtime=perhouseinfo['publishtime'],tag=perhouseinfo['tag'],unitprice=perhouseinfo['unitprice'],\
+                        totalprice=perhouseinfo['totalprice'])
+#            print(insertsql)
+            conn.execute(insertsql)
+            conn.commit()
+            
+#        except Exception as e:
+#            print(e)
+#            pass
+
+
+def createtable(db,tablename):
+    try:
+        conn = sqlite3.connect(db)
+    except Exception as e:
+        print ("Error: ",e)
+        sys.exit(1)
+    
+    droptablesql = 'drop table  if exists {tablename}'.format(tablename=tablename)
+#    createsql = 'create table {tablename} (title char, url char,followinfo char,flood char,houseinfo char,addres char,unitprice int,totalprice int,district char,type char,size int,face char,decoration char,buildtime char);'.format(tablename=TABLENAME)
+
+    createsql = 'create table {tablename} (url char, title char,district char,\
+                types char,size int,face char,decoration char,floor int,\
+                totalfloor int,position char,buildtime int,attention int,\
+                show int,publishtime char,tag char, totalprice int,unitprice int);'\
+                .format(tablename=tablename)
+
+
+
+    conn.execute(droptablesql)
+    conn.execute(createsql)
+#    conn.execute(insertsql)
+    conn.commit()
+
+
+
+if __name__ == '__main__':
+#    print(lianjia_totalpage(contents('http://gz.lianjia.com/ershoufang/')))
+#    for i in list(lianjia_perpage_info(3).keys()):
+#     print( [i for i in list(lianjia_perpage_info(3).keys())])
+#     print('insertsql = INTO {tablename} (i for i in list(lianjia_perpage_info(3).keys()))')
+
+    db = 'lianjia.db'
+    tablename = 'ershougz'
+    createtable(db,tablename)
+    
+    for page in range(101):
+        lianjia_perpage_info(page)
+        print("spider {} page".format(page))
+        
+    
